c345cf905d6fb6ee4dafdaafe4e55db3f5cb8c48
Marw diab nohamed <atefdiabmohamed@gmail.com>
2023-05-31 00:40:27 +0300

    Display financial months

--- app/Models/Finance_cln_periods.php
+++ app/Models/Finance_cln_periods.php
@@ -13,4 +13,14 @@ class Finance_cln_periods extends Model
     protected $fillable=[
         'finance_calenders_id', 'number_of_days', 'year_and_month', 'FINANCE_YR', 'MONTH_ID', 'START_DATE_M', 'END_DATE_M', 'is_open', 'start_date_for_pasma', 'end_date_for_pasma', 'added_by', 'updated', 'com_code', 'created_at', 'updated_at'
     ];
+
+    public function added(){
+        return $this->belongsTo('\App\Models\Admin','added_by');
+     }
+     public function updatedby(){
+        return $this->belongsTo('\App\Models\Admin','updated_by');
+     }
+     public function Month(){
+        return $this->belongsTo('\App\Models\Monthes','MONTH_ID');
+     }
 }
