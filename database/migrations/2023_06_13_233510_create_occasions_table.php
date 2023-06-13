f61d765fb113bd40442240e162d600be8b4e5420
Marw diab nohamed <atefdiabmohamed@gmail.com>
2023-06-14 00:49:57 +0300

    Create Occasions table

---
+++ database/migrations/2023_06_13_233510_create_occasions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('occasions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',225);
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->integer('days_counter');
+            $table->tinyInteger('active')->default(1);
+            $table->foreignId('added_by')->references('id')->on('admins')->onUpdate('cascade');
+            $table->foreignId('updated_by')->references('id')->on('admins')->onUpdate('cascade');
+            $table->integer('com_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('occasions');
+    }
+};
