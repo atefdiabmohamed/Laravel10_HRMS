bca768cfff43a530cf9dc2afe3ca1e5e65d69f18
Marw diab nohamed <atefdiabmohamed@gmail.com>
2023-08-22 02:47:24 +0300

    start Add new Employee

--- app/Http/Controllers/Admin/EmployeesController.php
+++ app/Http/Controllers/Admin/EmployeesController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Employee;
+
 class EmployeesController extends Controller
 {
-    public function index(){
-    $com_code=auth()->user()->com_code;
-    $data=get_cols_where_p(new Employee(),array("*"),array("com_code"=>$com_code),"id","DESC",PC);
-    return view("admin.Employees.index",['data'=>$data]);
+    public function index()
+    {
+        $com_code = auth()->user()->com_code;
+        $data = get_cols_where_p(new Employee(), array("*"), array("com_code" => $com_code), "id", "DESC", PC);
+        return view("admin.Employees.index", ['data' => $data]);
+    }
+    public function create(){
+        return view("admin.Employees.create");  
     }
 }
